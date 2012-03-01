2c654754de52e52aa3545c4a7d0f7ee69fc09385
richo <richo@psych0tik.net>
2012-03-01 11:22:45 +1100

    Whitespace

--- src/countkeys.cc
+++ src/countkeys.cc
@@ -5,7 +5,7 @@
   See COPYING for further information.
 
   This project was forked from Kernc's countkeys
-  
+
   Project homepage: https://github.com/richoH/countkeys
 */
 
@@ -85,22 +85,22 @@ void usr1_signal_handler(int interrupt) {
 }
 
 int main(int argc, char **argv) {
-  
-  
+
+
   if (geteuid()) { fprintf(stderr, "Got r00t?\n"); return EXIT_FAILURE; }
-  
+
   if (argc < 2) { usage(); return EXIT_FAILURE; }
-  
+
   bool flag_start = false;   // start logger
   bool flag_foreground = false;   // start logger
   // bool flag_kill; is defined global for signal_handler to access it
-  
+
   char *log_filename = (char*) DEFAULT_LOG_FILE;  // default log file
   char log_file_path[512]; // don't use paths longer than 512 B !!
   char *device_filename = NULL;  // path to input event device if given with -d switch
 
   { // process options and arguments
-    
+
     struct option long_options[] = {
       {"start",     no_argument,       0, 's'},
       {"output",    required_argument, 0, 'o'},
@@ -111,10 +111,10 @@ int main(int argc, char **argv) {
 #define EXPORT_KEYMAP_INDEX 7
       {0, 0, 0, 0}
     };
-    
+
     char c;
     int option_index;
-    
+
     while ((c = getopt_long(argc, argv, "sm:o:ukfd:?", long_options, &option_index)) != -1)
       switch (c) {
         case 's': flag_start = true;                            break;
@@ -122,10 +122,10 @@ int main(int argc, char **argv) {
         case 'k': flag_kill = true;                             break;
         case 'f': flag_foreground = true;                       break;
         case 'd': device_filename = optarg;                     break;
-        
-        case  0 : 
+
+        case  0 :
           break;  // + flag_export or flag_nofunc already set
-        
+
         case '?': usage(); return EXIT_SUCCESS;
         default : usage(); return EXIT_FAILURE;
       }
@@ -133,12 +133,12 @@ int main(int argc, char **argv) {
     while(optind < argc)
       fprintf(stderr, "%s: Non-option argument %s\n", argv[0], argv[optind++]);
   } //\ arguments
-  
+
   // kill existing countkeys process
   if (flag_kill) {
     FILE *temp_file = fopen(TMP_PID_FILE, "r");
     pid_t pid;
-    if ((temp_file != NULL && fscanf(temp_file, "%d", &pid) == 1 && fclose(temp_file) == 0) || 
+    if ((temp_file != NULL && fscanf(temp_file, "%d", &pid) == 1 && fclose(temp_file) == 0) ||
         (sscanf( exec("pgrep countkeys").c_str(), "%d", &pid) == 1 && pid != getpid())) { // if reading PID from temp_file failed, try pgrep pipe
       remove(TMP_PID_FILE);
       kill(pid, SIGINT);
@@ -146,13 +146,13 @@ int main(int argc, char **argv) {
     }
     fprintf(stderr, "%s: No process killed.\n", argv[0]);
     return EXIT_FAILURE;
-  
-  
+
+
   } else if (!flag_foreground && !flag_start) { usage(); return EXIT_FAILURE; }
   // if user provided a relative path to output file :/ stupid user :/
   if (log_filename[0] != '/') {
     if (getcwd(log_file_path, sizeof(log_file_path) - strlen(log_filename) - 2 /* '/' and '\0' */) == NULL) {
-      fprintf(stderr, "%s: Error copying CWD: %s%s\n", argv[0], strerror(errno), 
+      fprintf(stderr, "%s: Error copying CWD: %s%s\n", argv[0], strerror(errno),
         (errno == ERANGE ? " (CWD path too long, GO FUCK YOURSELF!!)" : ""));
       return EXIT_FAILURE;
     }
@@ -160,17 +160,17 @@ int main(int argc, char **argv) {
     strncat(log_file_path, log_filename, sizeof(log_file_path) - strlen(log_file_path));
     log_filename = log_file_path;
   }
-  
+
   // set locale to common UTF-8 for wchars to be recognized correctly
   if(setlocale(LC_CTYPE, "en_US.UTF-8") == NULL)  // if en_US.UTF-8 isn't available
     setlocale(LC_CTYPE, "");  // try the locale that corresponds to the value of the associated environment variables, LC_CTYPE and LANG
   // else just leave the burden of possible Fail to the user, without any warning :D
-  
-  
+
+
 #ifndef INPUT_EVENT_DEVICE  // sometimes X in /dev/input/eventX is different from one reboot to another
-  
+
   char *INPUT_EVENT_DEVICE;
-  
+
   if (device_filename == NULL) {  // no device given with -d switch, determine it automatically
     // extract input number from /proc/bus/input/devices (I don't know how to do it better. If you have an idea, please let me know.)
     std::string output = exec("grep Name /proc/bus/input/devices | grep -nE '[Kk]eyboard|kbd'");
@@ -178,20 +178,20 @@ int main(int argc, char **argv) {
       fprintf(stderr, "%s: Cannot determine keyboard input event device: %s\n", argv[0], strerror(errno));
       return EXIT_FAILURE;
     }
-    
+
     // the correct input event # is (output - 1)
     std::stringstream input_fd_filename;
     input_fd_filename << INPUT_EVENT_PATH;
     input_fd_filename << (atoi(output.c_str()) - 1);
-    
+
     INPUT_EVENT_DEVICE = (char*)input_fd_filename.str().c_str();
-    
+
   } else INPUT_EVENT_DEVICE = device_filename;  // event device supplied as -d argument
 
 #endif//INPUT_EVENT_DEVICE
-  
+
   { // catch SIGHUP, SIGINT and SIGTERM signals to exit gracefully
-    
+
     struct sigaction act_exit = {};
     struct sigaction act_usr1 = {};
     act_exit.sa_handler = exit_signal_handler;
@@ -202,9 +202,9 @@ int main(int argc, char **argv) {
     sigaction(SIGUSR1, &act_usr1, NULL);
 
   } //\ signals
-  
+
   { // everything went well up to now, let's become daemon
-    
+
    if (!flag_foreground) {
     switch (fork()) {
       case 0: break;  // child continues
@@ -217,7 +217,7 @@ int main(int argc, char **argv) {
     if(chdir("/"));  // wrapped in if() to avoid compiler warning
     switch (fork()) {
       case 0: break; // second child continues
-      case -1: 
+      case -1:
         fprintf(stderr, "%s: Error creating second child process: %s\n", argv[0], strerror(errno));
         return EXIT_FAILURE;
       default: return EXIT_SUCCESS;  // parent exits
@@ -225,7 +225,7 @@ int main(int argc, char **argv) {
     close(0);  // drop stdin
    }
   } //\ daemon
-  
+
   // create temp file carrying PID for later retrieval
   int temp_fd;
   if ((temp_fd = open(TMP_PID_FILE, O_WRONLY | O_CREAT, 0600)) == -1) {
@@ -246,14 +246,14 @@ int main(int argc, char **argv) {
     fprintf(stderr, "%s: Another process already running. Quitting.\n", argv[0]);
     return EXIT_FAILURE;
   } //\ temp file
-  
+
   // open input device for reading
   int input_fd = open(INPUT_EVENT_DEVICE, O_RDONLY | O_NONBLOCK);
   if (input_fd == -1) {
     fprintf(stderr, "%s: Error opening input event device '%s': %s\n", argv[0], INPUT_EVENT_DEVICE, strerror(errno));
     return EXIT_FAILURE;
   }
-  
+
   // open log file as stdout (if file doesn't exist, create it with safe 0600 permissions)
   //umask(0177); FIXME
   if (!flag_foreground) {
@@ -263,17 +263,17 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
       }
   }
-  
+
   // we've got everything we need, now drop privileges by becoming 'nobody'
   setgid(65534); setuid(65534);
-  
+
   struct input_event event;
   char timestamp[32];  // timestamp string, long enough to hold "\n%F %T%z > "
 
   time_t cur_time;
   time(&cur_time);
   strftime(timestamp, sizeof(timestamp), "%F %T%z", localtime(&cur_time));
-  
+
   fprintf(stdout, "Logging started ...%s\n\n", timestamp);
   fflush(stdout);
 
@@ -285,32 +285,32 @@ int main(int argc, char **argv) {
 #define EV_MAKE   1  // when key pressed
 #define EV_BREAK  0  // when key released
 #define EV_REPEAT 2  // when key switches to repeating after short delay
-    
+
     while (read(input_fd, &event, sizeof(struct input_event)) > 0) {
-      
+
       if (event.type == EV_KEY) {  // keyboard events always of type EV_KEY
-        
+
         // on key press
         if (event.value == EV_MAKE) {
             presses++;
         }
       }
     }
-    
+
     sleep(1);
   } //\ while (true)
-  
+
   // append final timestamp, close files and exit
   time(&cur_time);
   strftime(timestamp, sizeof(timestamp), "%F %T%z", localtime(&cur_time));
   fprintf(stdout, "Keys pressed: %i\n\n", presses);
   fprintf(stdout, "Logging stopped at %s\n", timestamp);
-  
+
   fclose(stdout);
   close(input_fd);
   close(temp_fd);
-  
+
   remove(TMP_PID_FILE);
-  
+
   return EXIT_SUCCESS;
 }
